3e20ee965be03c5e07b076de4b8f19a479fc563c
Supanut Dokmaithong <boomhack320@gmail.com>
2015-04-06 12:00:01 +0700

    แก้คำอธิบาย ในหน้า index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 echo "<h1>Welcome to mysqlexp module</h1>";
-echo "<h3>This module has two services.</h3>";
-echo "<ul><li>mysqlexp.php</li><li>practice.php</li></ul>";
+echo "<h3>This module has a services.</h3>";
+echo "<ul><li><a href='./mysqlexp.php'>mysqlexp.php</a></li></ul>";
 echo "Select to use services by directly link files.";
  ?>
